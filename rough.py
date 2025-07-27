4549adba47ac9ea03919b0dd4f1326c42623b43a
Ashish <itsashishmahajan@gmail.com>
2025-07-27 20:18:26 +0530

    add tricy loop

--- rough.py
+++ rough.py
@@ -5,46 +5,50 @@ for itervar in [3, 41, 12, 9, 74, 15]:
         smallest = itervar
         break
     print("Loop:", itervar, smallest)
-print("Smallest:", smallest)
+print("Smallest:", smallest) #op: Smallest:3
 
 
 if 0 is 0.0:
     print('wow')
 else:
-    print('oops')
+    print('oops')  #op: oops
 
 a=9
 
 if a is a:
     print('A wow')
 else:
-    print('A opps')
+    print('A opps') #op: A Wow
 
 
 message = 'The quick fox i-n the box.org'
 msg = message.split()
-print(msg)
+print(msg) #op: ['The', 'quick', 'fox', 'i-n', 'the', 'box.org']
 
 my_dict = {'Mon':1, 'Tue':2}
-print(list(my_dict.items()))
+print(list(my_dict.items())) #op: [('Mon', 1), ('Tue', 2)]
 
 my_dict['Wed'] = 3
 my_dict['Thu'] = 4
 my_dict['Mon'] = 11
 
-print(my_dict.get('Moon','Key not present'))
-print(my_dict.get('Moon'))
-print(my_dict.get('Mon'))
-print(my_dict)
+print(my_dict.get('Moon','Key not present')) #op: Key not present
+print(my_dict.get('Moon')) #op: None
+print(my_dict.get('Mon')) #op: 11
+print(my_dict) #op: {'Mon': 11, 'Tue': 2, 'Wed': 3, 'Thu': 4}
 
 for i in my_dict:
     print(i)
 
 nums=[1,2,3,4]
 
-print("sorted nums--->",sorted(nums)[1])
-print(nums[0:])
-print("nums[:2]",nums[:2])
+print("sorted nums--->",sorted(nums)[1]) #op: sorted nums---> 2
+print(nums[0:]) #op: [1,2,3,4]
+print("nums[:2]",nums[:2]) #op: [1,2]
 
+for i in range(len(nums)-1,-1,-1):
+    print(i,"=",nums[i])
 
+for i in range(len(nums)-1,-1,1):
+    print(i,"=",nums[i]) #op: No output
 
